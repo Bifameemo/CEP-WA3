fc8ae112637df4b180199357b75ab51162b17387
Bifameemo <95123055+Bifameemo@users.noreply.github.com>
2022-09-11 17:08:59 +0800

    Update product.py final

--- code/product.py
+++ code/product.py
@@ -1,7 +1,7 @@
-class Product():
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
+import random
 
-    def __str__(self):
-        return f"{self.name}: ${self.price}"
+class Product():
+    def __init__(self, name):
+        self.name = name
+        # sets a random price in the given range
+        self.price = random.choice(range(100, 9999))
