b41feb30bba396c4009a28e38ddea53e76cbbc82
Bifameemo <95123055+Bifameemo@users.noreply.github.com>
2022-09-11 17:05:57 +0800

    Update bus.py final

--- code/bus.py
+++ code/bus.py
@@ -1,90 +1,66 @@
-import random
-
-from customer import Customer
-
-names = ["Leam", "Olivia", "Noah", "Emma", "Oliver", "Charlotte", "Elijah", "Amelia"]
+import os
+os.system("color")
+# to print colored text later
 
 class Bus():
     def __init__(self):
-        self.day = 0
-        self.passengers = []
-        self.profits = 0
+        self.customers = []
 
-    def get_info(self):
-        for passenger in self.passengers:
-            print(f"{self.passengers.index(passenger) + 1}. {passenger}")
-        print()
+    # prints all Customers in the Bus
+    def display_customers(self):
+        print("|CUSTOMERS|".center(50).replace(" ", "="))
+        for index, customer in enumerate(self.customers):
+            print(f"{index + 1:>2}. {customer.name}")
 
-    def get_profits(self):
-        self.profits = 0
-        for customer in self.passengers:
-            for product in customer.fulfilled:
-                if product.split()[1] == "bought":
-                    self.profits -= int(product.split()[-1][1:])
-                elif product.split()[1] == "sold":
-                    self.profits += int(product.split()[-1][1:])
-        if self.profits > 0:
-            print(f"Today, you made a net profit of ${self.profits}")
-        elif self.profits < 0:
-            print(f"Today, you made a net loss of ${-self.profits}")
-        else: print("Nothing much happened today...")
-        print()
-
-    def start_new_day(self):
-        self.day += 1
-        self.passengers = [Customer(name) for name in random.sample(names, 4 + self.day // 2)]
-        print(f"{f'DAY-{self.day}':^50}".replace(" ", "="))
-        self.get_info()
-
-    def welcome_passengers(self, choices):
-        self.passengers = [self.passengers[index - 1] for index in choices]
-
-    def show_customers(self):
-        print(f"{'CUSTOMERS':^50}".replace(" ", "="))
-        self.get_info()
-
-    def show_customers_info(self):
-        for customer in self.passengers:
-            print(f"|{customer.name:^20}", end = "")
+    # prints all Customers in the Bus according to page along with their buylist and selllist
+    def display_customer_info(self, page):
+        # sets scope of Customers to only Customers on the page 
+        customers = self.customers[5 * (page - 1):5 * page]
+        # prints all Customers' names in a row
+        for customer in customers:
+            print(f"|{customer.name:^29}", end = "")
         print("|")
-        print(f"{'FULFILLED'}".center(len(self.passengers) * 21 + 1).replace(" ", "="))
-        for index in range(max([len(customer.fulfilled) for customer in self.passengers]) + 1):
-            for customer in self.passengers:
+        print("|BUYING|".center(len(customers) * 30 + 1).replace(" ", "="))
+        # method to find matching Products in buylists and selllists
+        all_lists = []
+        common_products = []
+        for customer in self.customers:
+            for product in customer.buylist + customer.selllist:
+                all_lists.append(product.name)
+        # if a Product appears multiple times in all_lists, then it is considered matching and added to common_products
+        for product in all_lists:
+            if all_lists.count(product) > 1:
+                common_products.append(product)
+        # loops through index based on the longest buylist
+        for index in range(max([len(customer.buylist) for customer in customers])):
+            for customer in customers:
+                # try except is used to avoid "list index out of range" errors
+                # if the index is in the range, it prints the Product details at that index
+                # if the index is out of the range, then there are no more Products to be printed, so it prints an empty column
                 try:
-                    print(f"|{str(customer.fulfilled[index]):<20}", end = "")
+                    product = customer.buylist[index]
+                    # if the Product is in the common_products, it is considered matching and then printed in yellow
+                    # this makes it easier for Players to find common Products between Customers' buylsits and selllists
+                    # otherwise, the Product's details are printed normally
+                    if product.name in common_products:
+                        print(f"|\033[1;33;40m{index + 1:>2}. {product.name:<16}\033[1;37;40m | ${product.price:<4} ", end = "")
+                    else:
+                        print(f"|{index + 1:>2}. {product.name:<16} | ${product.price:<4} ", end = "")
                 except:
-                    print(f"|{' ' * 20}", end = "")
+                    print(f"|{' ' * 29}", end = "")
             print("|")
-        print(f"{'BUYING'}".center(len(self.passengers) * 21 + 1).replace(" ", "="))
-        for index in range(max([len(customer.buylist) for customer in self.passengers]) + 1):
-            for customer in self.passengers:
+        print("|SELLING|".center(len(customers) * 30 + 1).replace(" ", "="))
+        # loops through and prints the same thing as the loop above, except for Customers' selllist instead of buylist
+        for index in range(max([len(customer.selllist) for customer in customers])):
+            for customer in customers:
                 try:
-                    print(f"|{str(customer.buylist[index]):<20}", end = "")
+                    product = customer.selllist[index]
+                    if product.name in common_products:
+                        print(f"|\033[1;33;40m{index + 1:>2}. {product.name:<16}\033[1;37;40m | ${product.price:<4} ", end = "")
+                    else:
+                        print(f"|{index + 1:>2}. {product.name:<16} | ${product.price:<4} ", end = "")
                 except:
-                    print(f"|{' ' * 20}", end = "")
+                    print(f"|{' ' * 29}", end = "")
             print("|")
-        print(f"{'SELLING'}".center(len(self.passengers) * 21 + 1).replace(" ", "="))
-        for index in range(max([len(customer.selllist) for customer in self.passengers]) + 1):
-            for customer in self.passengers:
-                try:
-                    print(f"|{str(customer.selllist[index]):<20}", end = "")
-                except:
-                    print(f"|{' ' * 20}", end = "")
-            print("|")
-
-    def display_bankruptcy(self):
-        print(f"{'BANKRUPTCY':^50}".replace(" ", "="))
-        print("YOU RAN OUT OF MONIES!!")
-        print()
-
-    def display_negligence(self):
-        print(f"{'NEGLIGENCE':^50}".replace(" ", "="))
-        print("You ngelected your customers too much...")
-        print()
-
-    def display_score_success(self):
-        print(f"{'SUCCESS':^50}".replace(" ", "="))
-        print("You started up a successful business!! CONGRATULATIONS")
-        print()
-        
-        
+        # prints the current page number out of the total number of pages
+        print(f"|Page-{page}/{int(-(-len(self.customers) / 5 // 1))}|".center(len(customers) * 30 + 1).replace(" ", "="))
