cf51feccf0e9af64866d947d959bf99ff9aae9bc
Bifameemo <95123055+Bifameemo@users.noreply.github.com>
2022-09-11 17:08:32 +0800

    Update player.py final

--- code/player.py
+++ code/player.py
@@ -1,35 +1,45 @@
 class Player():
-    def __init__(self, name):
-        self.name = name.capitalize()
-        self.balance = 500000
+    def __init__(self, full_name):
+        # formats name inputted, capitalises first letter of every word
+        self.name = "_".join([name.capitalize() for name in full_name.split()])
+        self.balance = 25000
         self.rating = 3
         self.score = 0
         self.stock = []
 
-    def get_info(self):
-        self.score = self.rating * 1000 + self.balance
-        print(f"Name: {self.name}")
-        print(f"Balance: ${self.balance}")
-        print(f"Rating: {self.rating}")
-        print(f"Score: {self.score}")
-        print(f"{'STOCK':^50}".replace(" ", "="))
-        print()
-        for item in self.stock:
-            print(item)
-        print("=" * 50)
-
-    def buy(self, product, customer):
-            self.stock.append(product.name)
-            self.balance -= product.price
-            customer.selllist.remove(product)
-            customer.fulfilled.append(f"You bought {product.name} for ${product.price}")
-
-    def sell(self, product, customer):
-        if product.name in self.stock:
-            self.stock.remove(product.name)
-            self.balance += product.price
-            customer.buylist.remove(product)
-            customer.fulfilled.append(f"You sold {product.name} for ${product.price}")
+    # prints all of Player's details
+    def display_info(self):
+        # calculates Player's score before printing
+        # 1 Rating is worth 5000 score, and then Player's balance is added on top of that
+        self.score = self.rating * 5000 + self.balance
+        print(f"|{self.name}|".center(50).replace(" ", "="))
+        print(f" Balance: ${self.balance}")
+        print(f" Rating: {self.rating}")
+        print(f" Score: {self.score}")
+        print("|STOCK|".center(50).replace(" ", "="))
+        # if nothing is in stock, prints "You have nothing in stock" instead of leaving an empty line
+        if len(self.stock) == 0:
+            print(" You have nothing in stock")
         else:
-            print("You don't have that!")
-            input()
+            for index, product in enumerate(self.stock):
+                print(f"{index + 1:>2}. {product}")
+
+    def buy_product_from(self, product, customer):
+        # adds the Product to the Player's stock
+        self.stock.append(product.name)
+        # reduces Player's balance by the Product's price
+        self.balance -= product.price
+        # removes the Product from the Customer's selllist
+        customer.selllist.remove(product)
+        # creates and adds a receipt to the Customer
+        customer.receipts.append(f"You bought {product.name} for ${product.price}")
+
+    def sell_product_to(self, product, customer):
+        # removes the Product from the Player's stock
+        self.stock.remove(product.name)
+        # increases the Player's balance by the Product's price
+        self.balance += product.price
+        # removes the Product from the Customer's buylist
+        customer.buylist.remove(product)
+        # creates and adds a receipt to the Customer
+        customer.receipts.append(f"You sold {product.name} for ${product.price}")
